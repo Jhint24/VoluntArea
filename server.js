a32edaa6481992ec8732c98fee8eff0bfc2eb7f4
zahra falah <sepidehfallah2@gmail.com>
2018-05-11 23:54:27 -0700

    setup first MVC Paradigm/handlebar

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override')
+// var path = require("path");
+
+
+
+// Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// static content
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+
+// Set Handlebars as the view engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+// var routes = require("./controllers/controller.js");
+// app.use(routes);
+
+app.listen(PORT, function() {
+  console.log("App now listening at localhost:" + PORT);
+});
