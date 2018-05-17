a0bc419851735869d02543f9cec0c277653dabc1
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-05-17 11:47:07 -0700

    added js for calendar selection. Still need to fix the bug to make the pick a date appear. Added a partials file for cards

--- public/assets/js/volJS.js
+++ public/assets/js/volJS.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
-    // // $('.carousel').carousel();
-    // $('.dropdown-trigger').dropdown();
-    // $('.datepicker').datepicker();
-    // $('#select1, #myselect').material_select();
 
-    // $("#select1").on('change', function () {
-    //     $('#myselect').val("1");
-    //     $('#myselect').material_select();
-    // });
     $('.carousel').carousel();
-    
-    var welcomeSection = $(' .welcome-section');
-    setTimeout (function () {
-        console.log("hi");
-        welcomeSection.removeClass('content-hidden');
-        console.log(welcomeSection);
-    }, 500);
+    var $input
+    //var picker 
+
+    $('.datepicker').datepicker();
+    var picker = $('.datepicker').val();
+    //console.log(picker)
+
+
+function getdate() {
+    var picker = $('.datepicker').val();
+    console.log(picker)
+
+}
+
+var welcomeSection = $(' .welcome-section');
+setTimeout(function () {
+    console.log("hi");
+    welcomeSection.removeClass('content-hidden');
+    console.log(welcomeSection);
+}, 500);
+
+
 });
