7b39f36c3f54335217c5670d65080878df06e46e
Nicole Santarsiero <santarsiero.nicole@gmail.com>
2018-05-19 23:09:38 -0700

    with added sorting for dates and card formatted

--- public/assets/js/volJS.js
+++ public/assets/js/volJS.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.timepicker').timepicker({
         twelveHour: false, // Use AM/PM or 24-hour format
     });
@@ -6,11 +6,12 @@ $(document).ready(function(){
 
 
     $('.carousel').carousel();
-  
+
 
     $('.datepicker').datepicker({
         format: 'yyyy-mm-dd'
     });
+
     var picker = $('.datepicker').val();
     //console.log(picker)
 
@@ -29,78 +30,112 @@ $(document).ready(function(){
     }, 500);
 
 
-        $("#getDate").click(function () {
-    // action goes here!!
-    var date = $('.datepicker').val();
-    console.log(date)
+    $("#getDate").click(function () {
+        //variable to get date selection value
+        var date = $('.datepicker').val();
+        console.log(date)
+        var result = [""];
+
+        //function to loop through date values of stored events
+        result = $('.eventDate').text();
+        var p = 0
+        var final = []
+        var temp = []
+        var j = 0
+        for (var i = 0; i < (result.length + 1); i++) {
+            if (i % 10 == 0) {
+                p = 0
+                final[j] = temp.join("")
+                console.log(final[j])
+                //console.log(j)
+                j = j + 1
+
+            }
+            temp[p] = result[i]
+            p = p + 1
+
+        }
+        
+        //to filter the events matching selected date
+        for (var i = 0; i < final.length; i++) {
+            console.log($(".card").find("[data-myVal]"));
+            if (String(date) != String(final[i])) {
+                $('#cardDiv' + (i)).hide();
+
+            }
+            else {
+                //console.log("match")
+            }
+        }
 
 
-});
 
-    
+    });
+
+
     $("#select1").on('change', function () {
         $('#myselect').val("1");
         $('#myselect').material_select();
     });
 
 
-//Activate submit button
-        $('#submitButton').on('click', function(event) {
+    //Activate submit button
+    $('#submitButton').on('click', function (event) {
         event.preventDefault();
         console.log(JSON.stringify("hi or whatever"));
 
-                // Gather user inputs
+        // Gather user inputs
         var userInput = {
             name: $('#userName').val(),//.trim() of undefined !!!
             web: $('.webAddress').val(),
-            activity:$('.activity').val(),
-            date:$('#volunteerdate').val(),
-            time:$('#volunteertime').val() + ":00", 
-            vol:$('.volNeeded').val(),         
-            hours:$('.eventHours').val()
-            
+            activity: $('.activity').val(),
+            date: $('#volunteerdate').val(),
+            time: $('#volunteertime').val() + ":00",
+            vol: $('.volNeeded').val(),
+            hours: $('.eventHours').val()
+
         };
         console.log(JSON.stringify(userInput));
-            // Add user inputs to orgs table
+        // Add user inputs to orgs table
 
-                $.post('/api/orgs', userInput)
-            .done(function(data) {
+        $.post('/api/orgs', userInput)
+            .done(function (data) {
                 console.log('JSON.stringify(data)');
-                
-    //     
-            
-            // Pop open the modal dialog
+
+                //     
+
+                // Pop open the modal dialog
                 $('#modal1').modal('open');
             });
 
-        });
+    });
 
-        //Activate submit button
-        $('#submitButton2').on('click', function(event) {
-            event.preventDefault();
-            console.log(JSON.stringify("HEYYYOYOYO"));
-    
-                    // Gather user inputs
-            var userInput = {
-                zip: $('#zipCode').val(),//.trim() of undefined !!!
-                name: $('#userName2').val(),
-                email:$('#emailAddress').val(),
-                
-            };
-            console.log(JSON.stringify(userInput));
-                // Add user inputs to orgs table
-    
-                    $.post('/api/vols', userInput)
-                .done(function(data) {
-                    console.log('JSON.stringify(data)');
-                    
-        //     
-                
+    //Activate submit button
+    $('#submitButton2').on('click', function (event) {
+        event.preventDefault();
+        console.log(JSON.stringify("HEYYYOYOYO"));
+
+        // Gather user inputs
+        var userInput = {
+            zip: $('#zipCode').val(),//.trim() of undefined !!!
+            name: $('#userName2').val(),
+            email: $('#emailAddress').val(),
+
+        };
+        console.log(JSON.stringify(userInput));
+        // Add user inputs to orgs table
+
+        $.post('/api/vols', userInput)
+            .done(function (data) {
+                console.log('JSON.stringify(data)');
+
+                //     
+
                 // Pop open the modal dialog
-                    $('#modal1').modal('open');
-                });
-    
+                $('#modal1').modal('open');
             });
+
+    });
 
 
 
